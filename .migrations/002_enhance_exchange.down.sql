bfa719cf9b789e61c6baeaff93e2ea969855236e
Uanderson Soares <2034724+uanderson@users.noreply.github.com>
2024-03-19 19:07:04 -0300

    chore: dusting off the digital shelves

--- .migrations/002_enhance_exchange.down.sql
+++
@@ -1,6 +0,0 @@
--- Resets the exchange rates table to the previous state
-ALTER TABLE exchange_rates DROP COLUMN created_at;
-ALTER TABLE exchange_rates ADD CONSTRAINT date_source_target_uq UNIQUE (date, source, target);
-
--- Drop the exchange currencies table
-DROP TABLE exchange_currencies;
--- .sqlc/bill/queries.sql
+++
@@ -1,2 +0,0 @@
--- name: GetRecurringBillsByRange :many
-SELECT * FROM recurring_bills WHERE start_at::date = @date AND start_at::date = @date AND end_at::date <= @date;
